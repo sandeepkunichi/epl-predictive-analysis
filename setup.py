fdd23f27d41cab6c5f345619d6cf729b78082c63
Sandeep Kunichi <sandeep.k@betsol.com>
2017-05-27 12:32:39 +0530

    Adding missing libs to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from subprocess import call
 call(['sudo','apt-get','install','python-pandas'])
 call(['sudo','apt-get','install','gfortran'])
+call(['sudo','apt-get','install','liblapack-dev'])
+call(['sudo','apt-get','install','python-sklearn']) 
 call(['sudo','easy_install','pymc'])
 call(['sudo','apt-get','install','python-wxgtk2.8'])
 print("Install complete. Run GUI.py from the ProductFinal package")
